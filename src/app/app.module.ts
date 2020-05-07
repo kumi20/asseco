76c782078f128f07b91469d84b16927cea97e54e
Jakub Kumięga <jkumiega@csgroup.pl>
2020-05-07 11:42:19 +0200

    poprawki stylistyczne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppServicesService } from './app-services.service';
-import { ProductComponent } from './product-list/product/product.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { PageNotFaoundComponent } from './page-not-faound/page-not-faound.component';
+import { AppServicesService } from "./app-services.service";
+import { ProductComponent } from "./product-list/product/product.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { PageNotFaoundComponent } from "./page-not-faound/page-not-faound.component";
+
+import {
+  DxTileViewModule,
+  DxButtonModule,
+  DxListModule,
+  DxAccordionModule,
+  DxCheckBoxModule,
+  DxSliderModule,
+  DxTagBoxModule,
+  DxTemplateModule,
+} from "devextreme-angular";
 
 @NgModule({
   declarations: [
@@ -24,16 +35,24 @@ import { PageNotFaoundComponent } from './page-not-faound/page-not-faound.compon
     ProductListComponent,
     ProductComponent,
     ProductDetailsComponent,
-    PageNotFaoundComponent
+    PageNotFaoundComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    DxButtonModule,
+    DxListModule,
+    DxTileViewModule,
+    DxAccordionModule,
+    DxCheckBoxModule,
+    DxSliderModule,
+    DxTagBoxModule,
+    DxTemplateModule
   ],
-  providers: [ AppServicesService ],
-  bootstrap: [AppComponent]
+  providers: [AppServicesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
